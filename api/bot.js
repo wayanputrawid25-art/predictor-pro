db2739fd63a8cf803dde6d5a5dd6ea25b1f8f562
wayanputrawid25-art <wayanputrawid25@gmail.com>
2026-04-28 16:05:48 +0800

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const TOKEN = process.env.TELEGRAM_TOKEN;
     const BASE_URL = process.env.BASE_URL;
