c79ae96eba55c63aa8e84964b162b9b931b6b87d
wayanputrawid25-art <wayanputrawid25@gmail.com>
2026-04-29 10:04:11 +0800

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -5,15 +5,12 @@ module.exports = async function handler(req, res) {
     const va = process.env.IPAYMU_VA;
     const apiKey = process.env.IPAYMU_API_KEY;
 
-    const ref = "TRX-" + Date.now();
-
     const payload = {
       product: ["Predictor Pro Elite"],
       qty: ["1"],
       price: ["15000"],
       returnUrl: "https://t.me/",
-      notifyUrl: process.env.BASE_URL + "/api/callback",
-      referenceId: ref
+      notifyUrl: process.env.BASE_URL + "/api/callback"
     };
 
     const body = JSON.stringify(payload);
@@ -22,8 +19,6 @@ module.exports = async function handler(req, res) {
       .createHmac("sha256", apiKey)
       .update(body)
       .digest("hex");
-
-    console.log("SIGNATURE:", signature);
 
     const response = await fetch("https://my.ipaymu.com/api/v2/payment", {
       method: "POST",
@@ -39,16 +34,6 @@ module.exports = async function handler(req, res) {
     const result = await response.json();
 
     console.log("IPAYMU:", result);
-
-    if (!result || result.Status !== 200) {
-      return res.status(200).json({
-        error: "ipaymu gagal",
-        detail: result
-      });
-    }
-
-    globalThis.orders = globalThis.orders || {};
-    globalThis.orders[ref] = req.query.chatId;
 
     return res.status(200).json(result);
 
